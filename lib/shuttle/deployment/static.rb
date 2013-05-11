4a6265b8c7a46ce53fa37897dddef4b7db523760
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-05-10 20:30:13 -0500

    Do not run hooks on static strategy, already included in core method

--- lib/shuttle/deployment/static.rb
+++ lib/shuttle/deployment/static.rb
@@ -2,14 +2,9 @@ module Shuttle
   class Static < Shuttle::Deploy
     def deploy
       setup
-
       update_code
       checkout_code
-
-      execute_hook(:before_link_release)
       link_release
-      execute_hook(:after_link_release)
-
       cleanup_releases
     end
   end
