1a431f9995afc13aee698358281c54470ab1956a
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-05-13 22:12:52 -0500

    Install node.js application dependencies

--- lib/shuttle/deployment/nodejs.rb
+++ lib/shuttle/deployment/nodejs.rb
@@ -2,12 +2,21 @@ module Shuttle
   class Nodejs < Shuttle::Deploy
     def setup
       if node_installed?
-        log "Using Node.js v#{node_version}, NPM v#{npm_version}"
+        log "Using Node.js v#{node_version}, Npm v#{npm_version}"
       else
         error "Node.js is not installed."
       end
 
       super
+    end
+
+    def deploy
+      setup
+      update_code
+      checkout_code
+      install_dependencies
+      link_release
+      cleanup_releases
     end
 
     private
@@ -22,6 +31,18 @@ module Shuttle
 
     def npm_version
       ssh.run("npm -v").output.strip
+    end
+
+    def install_dependencies
+      if ssh.file_exists?("#{release_path}/package.json")
+        log "Installing application dependencies"
+
+        result = ssh.run("cd #{release_path} && npm install")
+
+        if result.failure?
+          error "Unable to install dependencies: #{result.output}"
+        end
+      end
     end
   end
 end
