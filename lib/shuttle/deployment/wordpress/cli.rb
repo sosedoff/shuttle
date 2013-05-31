c3a46c32bd7cfff76708f9039284c582d3117be5
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-05-31 12:24:59 -0500

    Install wordpress cli from tags, not master

--- lib/shuttle/deployment/wordpress/cli.rb
+++ lib/shuttle/deployment/wordpress/cli.rb
@@ -15,10 +15,15 @@ module Shuttle
       log "Installing wordpress CLI"
 
       ssh.run("sudo git clone --recursive --quiet #{CLI_GIT} #{CLI_PATH}")
+      tags = ssh.capture("cd #{CLI_PATH} && git tag").split("\n").map(&:strip).reverse
+      tag  = tags.first
+      rev  = ssh.capture("cd #{CLI_PATH} && git rev-parse #{tag}").strip
+
+      ssh.run("cd #{CLI_PATH} && git checkout #{rev}")
       ssh.run("cd #{CLI_PATH} && sudo utils/dev-build")
       
       if cli_installed?
-        log "Wordpress CLI installed"
+        log "Wordpress CLI (#{tag}) installed"
       else
         error "Unable to install wordpress CLI"
       end
