3efff98505486b5827572949a8840e3a10a9a863
Dan Sosedoff <dan.sosedoff@gmail.com>
2013-07-07 23:09:12 -0500

    Add generator initial logic

--- lib/shuttle/generator.rb
+++ lib/shuttle/generator.rb
@@ -1,5 +1,55 @@
 module Shuttle
   class Generator
-    # TODO
+    include TerminalHelpers
+
+    STRATEGIES = %w(static wordpress rails)
+
+    attr_reader :name, :git, :path
+
+    def initialize(strategy='static')
+      unless STRATEGIES.include?(strategy)
+        raise ArgumentError, "Invalid strategy: #{strategy}"
+      end
+
+      @path = File.join(Dir.pwd, 'shuttle.yml')
+    end
+
+    def run
+      @name = ask('Application name', :required => true)
+      @git  = ask('Git repository', :required => true)
+
+      hash = send("generate_#{strategy}")
+
+      File.open(path, 'w') do |f|
+        f.write(YAML.dump(hash))
+      end
+
+      display "New shuttle config has been generated at ./shuttle.yml"
+    end
+
+    private
+
+    def generate_static
+      {
+        :app => {
+          :name => name,
+          :git  => git
+        },
+        :target => {
+          :host      => "mysite.com",
+          :user      => "deployer",
+          :password  => "password",
+          :deploy_to => "/home/deployer/#{name}"
+        }
+      }
+    end
+
+    def generate_wordpress
+      # TODO
+    end
+
+    def generate_rails
+      # TODO
+    end
   end
 end
