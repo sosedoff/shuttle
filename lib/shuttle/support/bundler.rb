34a0d0b018e6ae85e4a50c1f2c640368e02ac324
Dan Sosedoff <dan.sosedoff@gmail.com>
2014-03-31 09:54:09 -0500

    Add --retry flag to bundle install, retry 3 times

--- lib/shuttle/support/bundler.rb
+++ lib/shuttle/support/bundler.rb
@@ -30,6 +30,7 @@ module Shuttle
           "bundle install",
           "--quiet",
           "--path #{bundle_path}",
+          "--retry=3",
           "--binstubs",
           "--deployment"
         ].join(' ')
